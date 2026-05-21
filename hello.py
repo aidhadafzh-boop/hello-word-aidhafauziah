7cca9d9241dfb7bdc4a4155af554f76c59d21051
aidhadafzh-boop <aidhadafzh@gmail.com>
2026-05-21 09:24:02 +0700

    menambahkan hello word

---
+++ hello.py
@@ -0,0 +1,12 @@
+nama = "Aidha"
+nim = "241011402181"
+
+print("====================================")
+print("         PROJECT HELLO WORLD")
+print("====================================")
+print("Nama Mahasiswa :", nama)
+print("NIM            :", nim)
+print("====================================")
+print("Hello World!")
+print("Saya berhasil menjalankan Python.")
+print("====================================")
